91c1eb45105545189e9dc049040af53604d69df6
Advika <advikatejababji@gmail.com>
2023-09-26 22:09:56 +1000

    dwnwe ndwe

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/adveekurrr/github-classroom/rmit-computing-technologies/cosc2804-sep-23-assignment-1-Advika123456/mcpp
+# Build directory: /home/adveekurrr/github-classroom/rmit-computing-technologies/cosc2804-sep-23-assignment-1-Advika123456/mcpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("test")
---
+++ test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/adveekurrr/github-classroom/rmit-computing-technologies/cosc2804-sep-23-assignment-1-Advika123456/mcpp/test
+# Build directory: /home/adveekurrr/github-classroom/rmit-computing-technologies/cosc2804-sep-23-assignment-1-Advika123456/mcpp/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
